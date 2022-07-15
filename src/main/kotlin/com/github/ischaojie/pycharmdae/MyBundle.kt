e85ff00ee5707cbd502d82648203ab5695afdbb2
GitHub Action <action@github.com>
2022-07-15 04:39:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/ischaojie/pycharmdae/MyBundle.kt
+++ src/main/kotlin/com/github/ischaojie/pycharmdae/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ischaojie.pycharmdae
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ischaojie/pycharmdae/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ischaojie/pycharmdae/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ischaojie.pycharmdae.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ischaojie.pycharmdae.MyBundle
 
 class MyApplicationService {
 
